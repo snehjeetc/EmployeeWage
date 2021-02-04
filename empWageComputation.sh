a21a1775915ff334676a030b4f32b4b6987a72b5
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-04 19:06:11 +0530

    Refactor - daily payment functionality

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,11 +3,17 @@
 echo "Welcome to Employee Wage Computation Program"
 
 isPresent=1;
+WagePerHr=20
+fullDay=8
+dailyPayment=0
 checkRandom=$((RANDOM%2))
 if [ $isPresent -eq $checkRandom ]
 then
     echo "Employee is Present"
+    dailyPayment=$(($WagePerHr*$fullDay))
+    echo "Daily Payment is: " $dailyPayment
 else
     echo "Employee is absent"
+    echo "Daily Payment is: " $dailyPayment 
 fi
 
