82b65e6b293d61bf8cdf3a0f7d33548cea233a63
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-05 16:27:04 +0530

    Switch Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,18 +11,30 @@ fullDay=8
 partTimeHr=4
 dailyPayment=0
 checkRandom=$((RANDOM%2))
-if [ $present -eq $checkRandom ]
-then
-    echo "Employee is present"
-    jobType=$((RANDOM%2))
-    if [ $FullTime -eq $jobType ]
-    then
-        dailyPayment=$(($WagePerHr * $fullDay))
-    else
-        dailyPayment=$(($WagePerHr * $partTimeHr))
-    fi
-else
-    echo "Employee is absent"
-fi
+
+case $checkRandom in
+	$present)
+    		echo "Employee is present"
+    		jobType=$((RANDOM%2))
+    		case $jobType in
+    			$FullTime)
+        					dailyPayment=$(($WagePerHr * $fullDay))
+    						;;
+				$PartTime)
+        					dailyPayment=$(($WagePerHr * $partTimeHr))
+							;;
+						*)
+							echo "Invalid job type"
+							;;
+			esac
+			;;
+	$absent)
+    		echo "Employee is absent"
+			;;
+		  *)
+			echo "Invalid attendance"
+			;;
+esac
+
 
 echo "Daily Payment is : $dailyPayment"
