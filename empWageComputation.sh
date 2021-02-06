5d1bccb0a9e77f2027d4c8dde193f1b8aec5b95b
snehjeetc12 <csnehjeet12@gmail.com>
2021-02-06 17:46:24 +0530

    Added- function to record daily & total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -14,6 +14,11 @@ tot_WorkDays_limit=20
 tot_workingHrs_Limit=100
 
 #Daily Variables
+declare -a dailyWageRecord
+declare -a totalWageRecord
+dailyWageRecord[0]=0
+totalWageRecord[0]=0
+
 HrsWorked=0
 totDaysWorked=0
 dailyPayment=0
@@ -34,20 +39,29 @@ function getWorkingHrs(){
     echo $workingHrs
 }
 
+function addDailyRecords(){
+    index=$1
+    dailyWage_value=$2
+    totWage_value=$3
+    dailyWageRecord[$index]=$dailyWage_value
+    totWage_value[$index]=$totWage_value
+}
+
 while [[ $totDaysWorked -lt $tot_WorkDays_limit && 
         $HrsWorked -lt $tot_workingHrs_Limit ]]
 do
     checkRandom=$((RANDOM%2))
     case $checkRandom in
 	    $present)
-    		    echo "Employee is present"
+                echo "Employee is present"
                 workHr=$( getWorkingHrs $((RANDOM%2)) )
                 HrsWorked=$(($HrsWorked + $workHr))
                 dailyPayment=$(($WagePerHr*$workHr))
                 ((totDaysWorked++))
+                addDailyRecords $totDaysWorked $dailyPayment $salary
                 ;;
 	    $absent)
-    		    echo "Employee is absent"
+                echo "Employee is absent"
                 dailyPayment=0
 			    ;;
         *)
